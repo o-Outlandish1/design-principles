b763656a57ed3737196ddfec57aba35b77e2e8b2
andrewjspivey <12spivey@gmail.com>
2021-07-08 16:43:20 -0700

    ADD: adds golden rule styling

--- src/Components/GoldenRule/GoldenRule.js
+++ src/Components/GoldenRule/GoldenRule.js
@@ -4,21 +4,20 @@ import "./GoldenRule.css"
 const GoldenRule = () => {
 
     return (
-
         <div class="container">
-        <div class="item a">
-          <h1>GOLDEN RULE OF THIRDS</h1>
-          <h2>1 : 1.618</h2>
+            <div id="header" class="item a">
+                <h1>GOLDEN RULE OF THIRDS</h1>
+                <h2>1 : 1.618</h2>
+            </div>
+            <div class="item b">
+                <img id="fibonacci"src="https://mathigon.org/content/sequences/images/fibonacci.jpg" alt=""/>
+            </div>
+            <div class="item c"></div>
+            <div class="item d"></div>
+            <div class="item e"></div>
+            <div class="item f"></div>
+            <div class="item g"></div>
         </div>
-        <div class="item b">
-            <img id="fibonacci"src="https://mathigon.org/content/sequences/images/fibonacci.jpg" alt=""/>
-        </div>
-        <div class="item c">C</div>
-        <div class="item d">D</div>
-        <div class="item e">E</div>
-        <div class="item f">F</div>
-        <div class="item g">G</div>
-      </div>
     )
 }
 
