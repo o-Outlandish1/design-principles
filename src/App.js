edd5bb760777e8971189386ec79eb3fbaaeecff1
Robyn Beck <deft.or.die@gmail.com>
2021-07-08 13:42:43 -0700

    fixed App.js

--- src/App.js
+++ src/App.js
@@ -1,16 +1,10 @@
 import './App.css';
 import NavBar from './components/NavBar';
 
-import Flayout from './Components/Flayout/Flayout';
-
 function App() {
   return (
     <div className="App">
-<<<<<<< HEAD
-      <Flayout />
-=======
-      <NavBar/>
->>>>>>> main
+      <NavBar />
     </div>
   );
 }
