69a4c473ceeeef8a4bf5c18f3baa68be4eb8d22e
Akiko Green <akikogreen08@gmail.com>
2021-07-08 09:32:24 -0700

    neu & glass components created and has some starting content

--- src/Components/Glassmorphism/Glassmorphism.js
+++ src/Components/Glassmorphism/Glassmorphism.js
@@ -1,11 +1,20 @@
 import React from 'react';
+import './index.css'
 
 const Glassmorphism = () => {
-  return (
-    <div>
-      
-    </div>
-  );
+	return (
+		<div>
+			<div className="neuContainer">
+				<div className="descriptionBox">
+					<h1>NEUMORPHISM</h1>
+					<p />
+					<div className="exampleBox">{/* insert example picture of neumorphism */}</div>
+				</div>
+				<button className="neuButton1">Button 1</button>
+				<button className="neuButton2">Button 2</button>
+			</div>
+		</div>
+	);
 };
 
 export default Glassmorphism;
--- src/Components/Neumorphism/Neumorphism.js
+++ src/Components/Neumorphism/Neumorphism.js
@@ -1,9 +1,20 @@
 import React from 'react';
+import './index.css'
 
 const Neumorphism = () => {
   return (
     <div>
-      
+      <div className="neuContainer">
+        <div className="descriptionBox">
+          <h1>NEUMORPHISM</h1>
+          <p></p>
+          <div className="exampleBox">
+            {/* insert example picture of neumorphism */}
+          </div>
+        </div>
+        <button className="neuButton1">Button 1</button>
+        <button className="neuButton2">Button 2</button>
+      </div>
     </div>
   );
 };
