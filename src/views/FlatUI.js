676da34bf8fcfb17b7febe044c4e56bb40e95378
Robyn Beck <deft.or.die@gmail.com>
2021-07-08 16:34:30 -0700

    tweak button and pg layout

--- src/views/FlatUI.js
+++ src/views/FlatUI.js
@@ -4,7 +4,7 @@ import { Button } from "../components/FlatUI/index";
 export default class FlatUI extends React.Component {
   render() {
     return (
-      <div className="main">
+      <div className="flex">
         <Button />
       </div>
     );
