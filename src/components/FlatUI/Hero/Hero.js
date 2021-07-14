907138596cc4fe9e3e9cb4cbbf8c9f3fd20aae02
Robyn Beck <deft.or.die@gmail.com>
2021-07-13 17:07:29 -0700

    ADD: flat ui component to main branch

---
+++ src/components/FlatUI/Hero/Hero.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './Hero.css'
+
+export default function Hero(){
+  return (
+    <div className="hero-text">
+      <p className="this">this</p>
+      <p className="is">is</p>
+      <p className="flatUi"> FLAT UI</p>
+    </div>
+  );
+}
