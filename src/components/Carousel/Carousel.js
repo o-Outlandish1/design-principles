8c02d017274349acaa188d7b3d9f1da82055331d
Akiko Green <akikogreen08@gmail.com>
2021-07-16 10:32:48 -0700

    Carousel completed BETTER

--- src/components/Carousel/Carousel.js
+++ src/components/Carousel/Carousel.js
@@ -5,20 +5,37 @@ import {FaArrowAltCircleRight, FaArrowAltCircleLeft} from 'react-icons/fa'
 
 const Carousel = ({slides}) => {
  
-  const [currentImg, seCurrentImg] = useState(0)
+  const [currentImg, setCurrentImg] = useState(0)
   const length = images.length
+
+  if(!Array.isArray(images) || images.length <= 0){
+    return null
+  }
+
+  const nextSlide = () =>{
+    setCurrentImg(currentImg === length - 1 ? 0 : currentImg + 1)
+  }
+
+  const prevSlide = () =>{
+    setCurrentImg(currentImg === 0 ? length - 1 : currentImg - 1)
+  }
+
   return (
     <section className="slider">
       <FaArrowAltCircleLeft className="left-arrow" 
-      //onClick={prevSlide}
+      onClick={prevSlide}
        />
       <FaArrowAltCircleRight className="right-arrow" 
-      //onClick={nextSlide}
+      onClick={nextSlide}
        />
      {images.map((slide, index) =>{
        return (
-         <img src={slide.img} alt="Team 2 pics and memes" />
-       )
+         <div className={index === currentImg ? 'active slide' : 'slide'} key={index}>
+           {index === currentImg && ( 
+           <img src={slide.img} alt="Team 2 pics and memes" className="images"/>
+           )}
+         </div>
+         )
      })}
     </section>
   );
