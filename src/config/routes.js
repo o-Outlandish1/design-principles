97d2b7c1e9b6e4dbbc02b1eba5e40c18dccd8a5b
Robyn Beck <deft.or.die@gmail.com>
2021-07-19 12:50:25 -0700

    UPDATE: material-ui component

--- src/config/routes.js
+++ src/config/routes.js
@@ -1,15 +1,6 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
-
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
 import Carousel from '../components/Carousel/Carousel';
->>>>>>> 630a622a67ff009e1f20406c7be730b93f7b7586
-=======
-
-import Carousel from '../components/Carousel/Carousel';
->>>>>>> 70d5f903b7debf1ed90df54263d647f29b464101
 import GeneralPrinciples from '../components/GeneralPrinciples/GeneralPrinciple';
 import GoldenRules from '../components/GoldenRule/GoldenRule';
 import Flayout from "../components/Flayout/Flayout";
@@ -18,19 +9,9 @@ import Neumorphism from '../components/Neumorphism/Neumorphism';
 import Glassmorphism from '../components/Glassmorphism/Glassmorphism';
 // import your component here*
 import {FlatUI} from '../views/index';
-<<<<<<< HEAD
-<<<<<<< HEAD
-import {MaterialUI} from '../views/index';
-=======
-import NeuPinkMode from '../components/Neumorphism/NeuPinkMode';
-
->>>>>>> 630a622a67ff009e1f20406c7be730b93f7b7586
-
-=======
 import {MaterialUI} from '../views/index';
 import NeuPinkMode from '../components/Neumorphism/NeuPinkMode';
 
->>>>>>> 70d5f903b7debf1ed90df54263d647f29b464101
 export default (
   <Switch>
     <Route path="/general-principles" component={ GeneralPrinciples } />
@@ -40,18 +21,11 @@ export default (
     <Route path="/neumorphism" component={ Neumorphism } />
     <Route path="/glassmorphism" component={ Glassmorphism } />
     <Route path="/flat-ui" component={FlatUI} />
-<<<<<<< HEAD
-<<<<<<< HEAD
-    <Route path="/material-ui" component={ MaterialUI } />
-=======
-      {/* <Route path="/material-ui" component={ MaterialUI } /> */}
-      <Route path="/neu-pink-mode" component={NeuPinkMode}/>
-    <Route path="/" component={Carousel}/>
->>>>>>> 630a622a67ff009e1f20406c7be730b93f7b7586
-=======
     <Route path="/material-ui" component={ MaterialUI } />
     <Route path="/neu-pink-mode" component={NeuPinkMode}/>
     <Route path="/" component={Carousel}/>
->>>>>>> 70d5f903b7debf1ed90df54263d647f29b464101
+    <Route path="/material-ui" component={ MaterialUI } />
+    <Route path="/neu-pink-mode" component={NeuPinkMode}/>
+    <Route path="/" component={Carousel}/>
   </Switch>
 );
